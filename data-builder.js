61801a9b092f99a52310413e5695969c8f1e3303
Matt Pivnick <mpivnick@ebsco.com>
2018-02-21 07:21:19 -0500

    eh this needs more work

--- data-builder.js
+++ data-builder.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const request = require('request-promise');
 const cheerio = require('cheerio');
 
-//const url = 'http://www.azurilland.com/pokedex/gen-4/';
+//const url = 'http://www.azurilland.com/pokedex/gen-2/';
 const url = "https://www.azurilland.com/pokedex/gen-4/";
 const allPromises = [];
 
 const irregularMoves = require("./src/data/move-info.js");
 const damages = require("./src/data/damages.js");
-
+const errors = [];
 const editType = (type) => {
   let newType;
   const hashMap = {
@@ -92,185 +92,187 @@ const editEvolutionMethod = (methodText) => {
 
 const getPokemon = (id) => {
   return new Promise((resolve, reject) => {
-    return request(`${url}${id}`, (error, response, html) => {
-      if(!error){
-        const $ = cheerio.load(html);
+    setTimeout(() => {
+      const options = {
+        uri: `${url}${id}`,
+        timeout: 60000
+      };
+      return request(options, (error, response, html) => {
+        if (error) resolve(errors.push(error));
+        if(!error){
+          const $ = cheerio.load(html);
 
-        console.log(`Building info for ${id}`);
-        /* Name + Info*/
-        const regExp = /\(([^)]+)\)/;
-        const monsterName = $(".primary-content .header").eq(0).text();
-        const monsterSpecies = $(".species td").eq(1).text();
-        const monsterCatchRate = $(".pokemon-catch-rate").children().eq(1).text();
-        const monsterBaseExp = $(".pokemon-base-xp").children().eq(1).text();
-        const monsterBaseHappiness = $(".pokemon-base-happiness").children().eq(1).text();
-        const monsterHatchSteps = $(".pokemon-hatch-steps").children().eq(1).text();
-        const monsterGenderSplit = $(".pokemon-gender-group").children().eq(1).text();
-        const monsterGrowth = $(".pokemon-level-rate").children().eq(1).text();
-        const monsterEffortValues = $(".pokemon-effort-values").children().eq(1).text();
+          console.log(`Building info for ${id}`);
+          /* Name + Info*/
+          const monsterName = $(".primary-content .header").eq(0).text();
+          const monsterSpecies = $(".species td").eq(1).text();
+          const monsterCatchRate = $(".pokemon-catch-rate").children().eq(1).text();
+          //const monsterBaseExp = $(".pokemon-base-xp").children().eq(1).text();
+          const monsterBaseHappiness = $(".pokemon-base-happiness").children().eq(1).text();
+          const monsterHatchSteps = $(".pokemon-hatch-steps").children().eq(1).text();
+          //const monsterGenderSplit = $(".pokemon-gender-group").children().eq(1).text();
+          //const monsterGrowth = $(".pokemon-level-rate").children().eq(1).text();
+          //const monsterEffortValues = $(".pokemon-effort-values").children().eq(1).text();
+          /* End Name + Info */
 
-        const weightMatches = regExp.exec($(".pokemon-weight td").eq(1).text()) || [0, 10];
-        const monsterWeight = weightMatches[1];
-        // height needs some manipulation because it's 10x too big
-        const heightMatches = regExp.exec($(".pokemon-height td").eq(1).text()) || [0, 10]; // ex: "9m"
-        const monsterHeight = (parseInt(heightMatches[1], 10) / 10) + "m";
-        /* End Name + Info */
+          /* Types */
+          let monsterTypes = [];
+          let firstType = editType($(".primary-content .details").children(".item").eq(0).children().eq(0).text());
+          let secondType = editType($(".primary-content .details").children(".item").eq(0).children().eq(1).text());
 
-        /* Types */
-        let monsterTypes = [];
-        let firstType = editType($(".primary-content .details").children(".item").eq(0).children().eq(0).text());
-        let secondType = editType($(".primary-content .details").children(".item").eq(0).children().eq(1).text());
+          if (firstType !== secondType && secondType !== "") {
+            monsterTypes.push(firstType, secondType);
+          } else {
+            monsterTypes.push(firstType);
+          }
+          /* End Types */
 
-        if (firstType !== secondType && secondType !== "") {
-          monsterTypes.push(firstType, secondType);
-        } else {
-          monsterTypes.push(firstType);
-        }
-        /* End Types */
+          /* Base Stats */
+          let monsterStats = [];
+          const statElem = $(".primary-content .stats dl");
 
-        /* Base Stats */
-        let monsterStats = [];
-        const statElem = $(".primary-content .stats dl");
+          for (let i = 0; i < statElem.children().length / 2; i++) {
+            const statName = statElem.children(".name").eq(i).text();
+            const statValue = statElem.children(".value").eq(i).text();
 
-        for (let i = 0; i < statElem.children().length / 2; i++) {
-          const statName = statElem.children(".name").eq(i).text();
-          const statValue = statElem.children(".value").eq(i).text();
-
-          monsterStats.push({
-            statName,
-            statValue
-          });
-        }
-        /* End Base Stats */
-
-        /* Defensive Strengths/Weaknesses */
-        let monsterDefensive = [];
-        $(".primary-content .weaknesses-resistances ul").children().each((i, elem) => {
-          const typeName = editType($(elem).children(".tag").text());
-          const dmgMultiplier = editDmgMultiplier($(elem).children(".multiplier").text());
-
-          if (dmgMultiplier !== 1) {
-            monsterDefensive.push({
-              typeName,
-              dmgMultiplier
+            monsterStats.push({
+              statName,
+              statValue
             });
           }
-        });
-        // finally, sort by low => high
-        monsterDefensive.sort((a, b) => {
-          if (a.dmgMultiplier < b.dmgMultiplier) {
-            return -1;
-          } if (a.dmgMultiplier > b.dmgMultiplier) {
-            return 1;
-          }
-          return 0;
-        });
-        // const monsterDefensive = editDmgMultiplier(monsterTypes);
-        /* End Defensive Strengths/Weaknesses */
+          /* End Base Stats */
 
-        /* Moveset */
-        const monsterMoves = [];
-        for (let j = 0; j < $("#tab-moves").children(".listing-container").eq(0).find("tbody").children().length; j++ ) {
-          const elem = $("#tab-moves").children(".listing-container").eq(0).find("tbody").children().eq(j);
-          let moveData = {};
-          for (let i = 0; i < elem.children().length; i++) {
-            const moveName = elem.children().eq(1).text();
-            const movePowerInfo = editMovePower(moveName, elem.children().eq(4).text().trim());
-            //console.log(movePowerInfo);
-            moveData.level = elem.children().eq(0).text();
-            moveData.name = moveName;
-            moveData.type = editType(elem.children().eq(2).text());
-            moveData.category = elem.children().eq(3).text();
-            moveData.power = elem.children().eq(4).find("strong").text() || movePowerInfo.movePower;
-            moveData.accuracy = elem.children().eq(5).text();
-            moveData.pp = elem.children().eq(6).text();
-            if ( movePowerInfo.moveNote ) {
-              moveData.notes = movePowerInfo.moveNote;
+          /* Defensive Strengths/Weaknesses */
+          let monsterDefensive = [];
+          $(".primary-content .weaknesses-resistances ul").children().each((i, elem) => {
+            const typeName = editType($(elem).children(".tag").text());
+            const dmgMultiplier = editDmgMultiplier($(elem).children(".multiplier").text());
+
+            if (dmgMultiplier !== 1) {
+              monsterDefensive.push({
+                typeName,
+                dmgMultiplier
+              });
             }
+          });
+          // finally, sort by low => high
+          monsterDefensive.sort((a, b) => {
+            if (a.dmgMultiplier < b.dmgMultiplier) {
+              return -1;
+            } if (a.dmgMultiplier > b.dmgMultiplier) {
+              return 1;
+            }
+            return 0;
+          });
+          // const monsterDefensive = editDmgMultiplier(monsterTypes);
+          /* End Defensive Strengths/Weaknesses */
+
+          /* Moveset */
+          const monsterMoves = [];
+          for (let j = 0; j < $("#tab-moves").children(".listing-container").eq(0).find("tbody").children().length; j++ ) {
+            const elem = $("#tab-moves").children(".listing-container").eq(0).find("tbody").children().eq(j);
+            let moveData = {};
+            for (let i = 0; i < elem.children().length; i++) {
+              const moveName = elem.children().eq(1).text();
+              const movePowerInfo = editMovePower(moveName, elem.children().eq(4).text().trim());
+              //console.log(movePowerInfo);
+              moveData.level = elem.children().eq(0).text();
+              moveData.name = moveName;
+              moveData.type = editType(elem.children().eq(2).text());
+              moveData.category = elem.children().eq(3).text();
+              moveData.power = elem.children().eq(4).find("strong").text() || movePowerInfo.movePower;
+              moveData.accuracy = elem.children().eq(5).text();
+              moveData.pp = elem.children().eq(6).text();
+              if ( movePowerInfo.moveNote ) {
+                moveData.notes = movePowerInfo.moveNote;
+              }
+            }
+
+            monsterMoves.push(moveData);
+          }
+          /* End Moveset */
+  /*
+          // get evolutionary info =/
+          const evoList = $(".evolution");
+          const monsterEvolutions = [];
+          // there may be more than one evolution section if
+          // a pokemon mega evolves...
+          const evoTree = evoList.length > 1
+            ?
+              evoList.eq(1)
+            :
+              evoList;
+
+          monsterEvolutions.push([{
+            stage: 1,
+            name: evoTree.children(".base-evo").text().trim(), // "slowpoke",
+            id: evoTree.children(".base-evo").find("a").eq(0).attr("href").split("/")[3].split("-")[0] // uhh...
+          }]);
+
+          let temp = []
+
+          for (let i = 0; i < evoTree.children(".second-evo").length; i++) {
+            temp.push({
+              stage: 2,
+              name: evoTree.children(".second-evo").eq(i).text().trim().match(/Evolves to\s(\w*)/, "")[1],
+              method: editEvolutionMethod(evoTree.children(".second-evo").eq(i).text().trim()),
+              id: evoTree.children(".second-evo").eq(i).find("a").eq(0).attr("href").split("/")[3].split("-")[0]
+            });
+          }
+          if (temp.length) {
+            monsterEvolutions.push(temp);
           }
 
-          monsterMoves.push(moveData);
-        }
-        /* End Moveset */
-/*
-        // get evolutionary info =/
-        const evoList = $(".evolution");
-        const monsterEvolutions = [];
-        // there may be more than one evolution section if
-        // a pokemon mega evolves...
-        const evoTree = evoList.length > 1
-          ?
-            evoList.eq(1)
-          :
-            evoList;
-
-        monsterEvolutions.push([{
-          stage: 1,
-          name: evoTree.children(".base-evo").text().trim(), // "slowpoke",
-          id: evoTree.children(".base-evo").find("a").eq(0).attr("href").split("/")[3].split("-")[0] // uhh...
-        }]);
-
-        let temp = []
-
-        for (let i = 0; i < evoTree.children(".second-evo").length; i++) {
-          temp.push({
-            stage: 2,
-            name: evoTree.children(".second-evo").eq(i).text().trim().match(/Evolves to\s(\w*)/, "")[1],
-            method: editEvolutionMethod(evoTree.children(".second-evo").eq(i).text().trim()),
-            id: evoTree.children(".second-evo").eq(i).find("a").eq(0).attr("href").split("/")[3].split("-")[0]
+          temp = [];
+          for (let i = 0; i < evoTree.children(".third-evo").length; i++) {
+            temp.push({
+              stage: 3,
+              name: evoTree.children(".third-evo").eq(i).text().trim().match(/Evolves to\s(\w*)/, "")[1],
+              method: editEvolutionMethod(evoTree.children(".third-evo").eq(i).text().trim()),
+              id: evoTree.children(".third-evo").eq(i).find("a").eq(0).attr("href").split("/")[3].split("-")[0]
+            });
+          }
+          if (temp.length) {
+            monsterEvolutions.push(temp);
+          }
+  */
+          resolve({
+            id,
+            monsterName,
+            monsterSpecies,
+            monsterTypes,
+            monsterStats,
+            monsterDefensive,
+            monsterMoves,
+            //monsterEvolutions,
+            monsterCatchRate,
+            //monsterBaseExp,
+            monsterBaseHappiness,
+            monsterHatchSteps//,
+            //monsterGenderSplit,
+            //monsterGrowth,
+            //monsterEffortValues
           });
         }
-        if (temp.length) {
-          monsterEvolutions.push(temp);
-        }
-
-        temp = [];
-        for (let i = 0; i < evoTree.children(".third-evo").length; i++) {
-          temp.push({
-            stage: 3,
-            name: evoTree.children(".third-evo").eq(i).text().trim().match(/Evolves to\s(\w*)/, "")[1],
-            method: editEvolutionMethod(evoTree.children(".third-evo").eq(i).text().trim()),
-            id: evoTree.children(".third-evo").eq(i).find("a").eq(0).attr("href").split("/")[3].split("-")[0]
-          });
-        }
-        if (temp.length) {
-          monsterEvolutions.push(temp);
-        }
-*/
-        resolve({
-          id,
-          monsterName,
-          monsterSpecies,
-          monsterWeight,
-          monsterHeight,
-          monsterTypes,
-          monsterStats,
-          monsterDefensive,
-          monsterMoves,
-          //monsterEvolutions,
-          monsterCatchRate,
-          monsterBaseExp,
-          monsterBaseHappiness,
-          monsterHatchSteps,
-          monsterGenderSplit,
-          monsterGrowth,
-          monsterEffortValues
-        });
-      }
-    });
+      });
+    }, 300 * id);
   });
 };
-
-
-for (let i = 1; i < 494; i++) {
+//1-100 (101)
+//101-200 (201)
+//201-300 (301)
+//301-400 (401)
+//401-493 (494)
+for (let i = 101; i < 201; i++) {
   allPromises.push(getPokemon(i));
 }
 
 Promise.all(allPromises).then(values => {
-  console.log(values);
-  fs.writeFile("src/data/monsters2.json", JSON.stringify(values), err => {
+  console.log(values.id);
+  fs.writeFile("src/data/monsters200.json", JSON.stringify(values), err => {
     if (err) return console.log(err);
     console.log("Monsters.json written successfully");
+    console.log(errors); // eslint-disable-line
   });
 }).catch(reason => {
   console.log(reason)
