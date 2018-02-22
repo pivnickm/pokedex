bd2ab707cb50e0e6b4ead007ebee42792a79bc56
Matt Pivnick <mpivnick@ebsco.com>
2018-02-22 15:27:24 -0500

    using firebase, and now getting fresher data for use in firebase db

--- data-builder.js
+++ data-builder.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const request = require('request-promise');
 const cheerio = require('cheerio');
 
-const url = 'http://www.azurilland.com/pokedex/gen-2/';
+// const url = 'http://www.azurilland.com/pokedex/gen-2/';
+const url = 'https://www.serebii.net/pokedex-dp/';
 const allPromises = [];
 
 const irregularMoves = require("./src/data/move-info.js");
@@ -91,150 +92,21 @@ const editEvolutionMethod = (methodText) => {
 
 const getPokemon = (id) => {
   return new Promise((resolve, reject) => {
-    return request(`${url}${id}`, (error, response, html) => {
+    return request(`${url}${id}.shtml`, (error, response, html) => {
       if(!error){
         const $ = cheerio.load(html);
-
+        console.log(`${url}${id}.shtml`); // eslint-disable-line
         console.log(`Building info for ${id}`);
         /* Name + Info*/
-        const regExp = /\(([^)]+)\)/;
-        const monsterName = $(".primary-content .header").eq(0).text();
-        const monsterSpecies = $(".species td").eq(1).text();
-        const monsterCatchRate = $(".pokemon-catch-rate").children().eq(1).text();
-        const monsterBaseExp = $(".pokemon-base-xp").children().eq(1).text();
-        const monsterBaseHappiness = $(".pokemon-base-happiness").children().eq(1).text();
-        const monsterHatchSteps = $(".pokemon-hatch-steps").children().eq(1).text();
-        const monsterGenderSplit = $(".pokemon-gender-group").children().eq(1).text();
-        const monsterGrowth = $(".pokemon-level-rate").children().eq(1).text();
-        const monsterEffortValues = $(".pokemon-effort-values").children().eq(1).text();
-
-        const weightMatches = regExp.exec($(".pokemon-weight td").eq(1).text());
-        const monsterWeight = weightMatches[1];
-        // height needs some manipulation because it's 10x too big
-        const heightMatches = regExp.exec($(".pokemon-height td").eq(1).text()); // ex: "9m"
-        const monsterHeight = (parseInt(heightMatches[1], 10) / 10) + "m";
+        const monsterName = $(".fooinfo").eq(0).text();
+        const monsterSpecies = $(".fooinfo").eq(6).text();
+        const monsterCatchRate = $(".fooinfo").eq(9).text();
+        const monsterBaseHappiness = $(".fooinfo").eq(12).text();
+        const monsterHatchSteps = $(".fooinfo").eq(10).text();
+        const monsterGenderSplit = $(".fooinfo").eq(3).text();
+        const monsterWeight = $(".fooinfo").eq(8).text();
+        const monsterHeight = $(".fooinfo").eq(7).text();
         /* End Name + Info */
-
-        /* Types */
-        let monsterTypes = [];
-        let firstType = editType($(".primary-content .details").children(".item").eq(0).children().eq(0).text());
-        let secondType = editType($(".primary-content .details").children(".item").eq(0).children().eq(1).text());
-
-        if (firstType !== secondType && secondType !== "") {
-          monsterTypes.push(firstType, secondType);
-        } else {
-          monsterTypes.push(firstType);
-        }
-        /* End Types */
-
-        /* Base Stats */
-        let monsterStats = [];
-        const statElem = $(".primary-content .stats dl");
-
-        for (let i = 0; i < statElem.children().length / 2; i++) {
-          const statName = statElem.children(".name").eq(i).text();
-          const statValue = statElem.children(".value").eq(i).text();
-
-          monsterStats.push({
-            statName,
-            statValue
-          });
-        }
-        /* End Base Stats */
-
-        /* Defensive Strengths/Weaknesses */
-        let monsterDefensive = [];
-        $(".primary-content .weaknesses-resistances ul").children().each((i, elem) => {
-          const typeName = editType($(elem).children(".tag").text());
-          const dmgMultiplier = editDmgMultiplier($(elem).children(".multiplier").text());
-
-          if (dmgMultiplier !== 1) {
-            monsterDefensive.push({
-              typeName,
-              dmgMultiplier
-            });
-          }
-        });
-        // finally, sort by low => high
-        monsterDefensive.sort((a, b) => {
-          if (a.dmgMultiplier < b.dmgMultiplier) {
-            return -1;
-          } if (a.dmgMultiplier > b.dmgMultiplier) {
-            return 1;
-          }
-          return 0;
-        });
-        // const monsterDefensive = editDmgMultiplier(monsterTypes);
-        /* End Defensive Strengths/Weaknesses */
-
-        /* Moveset */
-        const monsterMoves = [];
-        for (let j = 0; j < $("#tab-moves").children(".listing-container").eq(0).find("tbody").children().length; j++ ) {
-          const elem = $("#tab-moves").children(".listing-container").eq(0).find("tbody").children().eq(j);
-          let moveData = {};
-          for (let i = 0; i < elem.children().length; i++) {
-            const moveName = elem.children().eq(1).text();
-            const movePowerInfo = editMovePower(moveName, elem.children().eq(4).text().trim());
-            //console.log(movePowerInfo);
-            moveData.level = elem.children().eq(0).text();
-            moveData.name = moveName;
-            moveData.type = editType(elem.children().eq(2).text());
-            moveData.category = elem.children().eq(3).text();
-            moveData.power = elem.children().eq(4).find("strong").text() || movePowerInfo.movePower;
-            moveData.accuracy = elem.children().eq(5).text();
-            moveData.pp = elem.children().eq(6).text();
-            if ( movePowerInfo.moveNote ) {
-              moveData.notes = movePowerInfo.moveNote;
-            }
-          }
-
-          monsterMoves.push(moveData);
-        }
-        /* End Moveset */
-
-        // get evolutionary info =/
-        const evoList = $(".evolution");
-        const monsterEvolutions = [];
-        // there may be more than one evolution section if
-        // a pokemon mega evolves...
-        const evoTree = evoList.length > 1
-          ?
-            evoList.eq(1)
-          :
-            evoList;
-
-        monsterEvolutions.push([{
-          stage: 1,
-          name: evoTree.children(".base-evo").text().trim(), // "slowpoke",
-          id: evoTree.children(".base-evo").find("a").eq(0).attr("href").split("/")[3].split("-")[0] // uhh...
-        }]);
-
-        let temp = []
-
-        for (let i = 0; i < evoTree.children(".second-evo").length; i++) {
-          temp.push({
-            stage: 2,
-            name: evoTree.children(".second-evo").eq(i).text().trim().match(/Evolves to\s(\w*)/, "")[1],
-            method: editEvolutionMethod(evoTree.children(".second-evo").eq(i).text().trim()),
-            id: evoTree.children(".second-evo").eq(i).find("a").eq(0).attr("href").split("/")[3].split("-")[0]
-          });
-        }
-        if (temp.length) {
-          monsterEvolutions.push(temp);
-        }
-
-        temp = [];
-        for (let i = 0; i < evoTree.children(".third-evo").length; i++) {
-          temp.push({
-            stage: 3,
-            name: evoTree.children(".third-evo").eq(i).text().trim().match(/Evolves to\s(\w*)/, "")[1],
-            method: editEvolutionMethod(evoTree.children(".third-evo").eq(i).text().trim()),
-            id: evoTree.children(".third-evo").eq(i).find("a").eq(0).attr("href").split("/")[3].split("-")[0]
-          });
-        }
-        if (temp.length) {
-          monsterEvolutions.push(temp);
-        }
 
         resolve({
           id,
@@ -242,18 +114,10 @@ const getPokemon = (id) => {
           monsterSpecies,
           monsterWeight,
           monsterHeight,
-          monsterTypes,
-          monsterStats,
-          monsterDefensive,
-          monsterMoves,
-          monsterEvolutions,
           monsterCatchRate,
-          monsterBaseExp,
           monsterBaseHappiness,
           monsterHatchSteps,
-          monsterGenderSplit,
-          monsterGrowth,
-          monsterEffortValues
+          monsterGenderSplit
         });
       }
     });
@@ -261,13 +125,14 @@ const getPokemon = (id) => {
 };
 
 
-for (let i = 1; i < 252; i++) {
-  allPromises.push(getPokemon(i));
+for (let i = 1; i < 2; i++) {
+  const id = ("0000" + i).substr(-3, 3);
+  allPromises.push(getPokemon(id));
 }
 
 Promise.all(allPromises).then(values => {
   console.log(values);
-  fs.writeFile("src/data/monsters.json", JSON.stringify(values), err => {
+  fs.writeFile("src/data/monsters2.json", JSON.stringify(values), err => {
     if (err) return console.log(err);
     console.log("Monsters.json written successfully");
   });
--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import { Router, browserHistory } from "react-router";
-import { useBasename } from 'history';
 import routes from "./routes";
 
 class App extends Component {
   render () {
     return (
-      <Router history={useBasename(() => browserHistory)({ basename: '/pokedex' })}>
+      <Router history={browserHistory}>
         {routes}
       </Router>
     );
